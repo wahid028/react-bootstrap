e7a6f6594fb0481e53340cbc765b7c82dfdd46a6
wahid028 <wahid028@gmail.com>
2022-01-21 17:04:37 -0800

    Routing fix

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Pages/HomePage";
+import Member from "./components/Pages/MemberPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/member" element={<Member />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
---
+++ src/components/Pages/MemberPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Container, Row, Card, CardGroup, Button } from "react-bootstrap";
+
+const wrapper = {
+  width: "100%",
+  height: "93vh",
+};
+
+const Member = () => {
+  return (
+    <div style={wrapper}>
+      <Container>
+        <Row>
+          <CardGroup>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE"
+              />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button> Read More </Button>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo"
+              />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button> Read More </Button>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://i.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I"
+              />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This card has even longer
+                  content than the first to show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button> Read More </Button>
+              </Card.Footer>
+            </Card>
+          </CardGroup>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Member;
